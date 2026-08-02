dacd5ad6ab43b6bf1ecb418624ede887b7b26ff5
Indhira Iyappan <indhiraiyappan17@gmail.com>
2026-08-02 20:26:37 +0530

    js changing

--- js/dash.js
+++ js/dash.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const expenseElement = document.getElementById("totalExpense");
     const balanceElement = document.getElementById("balance");
     const transactionElement = document.getElementById("totalTransactions");
+    const recentTransactions = document.getElementById("recentTransactions");
 
     const totals = calculateTotals();
     const transactions = getTransactions();
@@ -22,6 +23,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (transactionElement) {
         transactionElement.textContent = transactions.length;
+    }
+
+    // Show Recent Transactions
+    if (recentTransactions) {
+
+        recentTransactions.innerHTML = "";
+
+        if (transactions.length === 0) {
+            recentTransactions.innerHTML =
+                `<tr><td colspan="4">No Transactions Available</td></tr>`;
+        } else {
+
+            transactions.slice(-5).reverse().forEach(function(item) {
+
+                recentTransactions.innerHTML += `
+                    <tr>
+                        <td>${item.date}</td>
+                        <td>${item.category}</td>
+                        <td>${item.type}</td>
+                        <td>₹${item.amount}</td>
+                    </tr>
+                `;
+
+            });
+
+        }
     }
 
 });
